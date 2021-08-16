4f3f5c855949398475832f3c730294594d4da5a9
Király Tamás <tomaszpest@gmail.com>
2021-08-16 18:57:28 +0200

    Haladunk a feladattal

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById('myCanvas');
-canvas.width = document.getElementById('myCanvas').clientWidth;
-canvas.height = document.getElementById('myCanvas').clientHeight;
+var canvaswidth = document.getElementById('myCanvas').clientWidth;
+var canvasheight = document.getElementById('myCanvas').clientHeight;
+canvas.width=canvaswidth;
+canvas.height=canvasheight;
 var context = canvas.getContext('2d');
 context.fillStyle = 'orange';
 context.fillRect(175, 100, 100, 100);
@@ -9,3 +11,6 @@ context.moveTo(0, 300);
 context.lineTo(450, 0);
 context.strokeStyle = 'red';
 context.stroke();
+context.beginPath();
+context.fillStyle='black';
+context.fillRect(canvas.width/2,canvas.height/2,canvas.width/2,canvas.height/2)
